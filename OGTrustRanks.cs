dcd9fb212128d0aefce498cfbd583185516d60fb
abbeybabbey <61197745+abbeybabbey@users.noreply.github.com>
2021-11-01 22:10:11 +0100

    made changes to comply with vrmg
    - removed vrchat team
    - added back showSocialRankMethod check
    - also fixes the notorious visitor user bug in social list when trying to search for someone (https://i.imgur.com/OfbQqi3.png)
    - added in my opinion better nuisance rank
    - you can customize colors for all trust ranks
    - simplified some methods
    I was lazy to better format the code and also setuptrustrankbutton method has object reference error

--- OGTrustRanks.cs
+++ OGTrustRanks.cs
@@ -1,10 +1,11 @@
-using HarmonyLib;
-using MelonLoader;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using HarmonyLib;
+using MelonLoader;
+using UnhollowerRuntimeLib.XrefScans;
 using UnityEngine;
 using UnityEngine.UI;
 using VRC;
@@ -18,24 +19,24 @@ namespace OGTrustRanks
         public const string Name = "OGTrustRanks";
         public const string Author = "Herp Derpinstine, Emilia, dave-kun, and Requi";
         public const string Company = "Lava Gang";
-        public const string Version = "1.1.5";
+        public const string Version = "1.1.6";
         public const string DownloadLink = "https://github.com/RequiDev/OGTrustRanks";
     }
 
     public class OGTrustRanks : MelonMod
     {
-        private static Color _vrchatTeamColor;
         private static Color _legendColor;
         private static Color _veteranUserColor;
-        private static Color _knownUserColor;
         private static Color _trustedUserColor;
+        private static Color _knownUserColor;
+        private static Color _userUserColor;
+        private static Color _newUserColor;
+        private static Color _visitorColor;
+        private static Color _nuisanceColor;
         private static MelonPreferences_Entry<bool> _enabledPref;
-        private static MelonPreferences_Entry<int> _vrchatTeamColorRPref;
-        private static MelonPreferences_Entry<int> _vrchatTeamColorGPref;
-        private static MelonPreferences_Entry<int> _vrchatTeamColorBPref;
-        private static MelonPreferences_Entry<int> _legendaryColorRPref;
-        private static MelonPreferences_Entry<int> _legendaryColorGPref;
-        private static MelonPreferences_Entry<int> _legendaryColorBPref;
+        private static MelonPreferences_Entry<int> _legendColorRPref;
+        private static MelonPreferences_Entry<int> _legendColorGPref;
+        private static MelonPreferences_Entry<int> _legendColorBPref;
         private static MelonPreferences_Entry<int> _veteranColorRPref;
         private static MelonPreferences_Entry<int> _veteranColorGPref;
         private static MelonPreferences_Entry<int> _veteranColorBPref;
@@ -45,11 +46,24 @@ namespace OGTrustRanks
         private static MelonPreferences_Entry<int> _knownColorRPref;
         private static MelonPreferences_Entry<int> _knownColorGPref;
         private static MelonPreferences_Entry<int> _knownColorBPref;
+        private static MelonPreferences_Entry<int> _userColorRPref;
+        private static MelonPreferences_Entry<int> _userColorGPref;
+        private static MelonPreferences_Entry<int> _userColorBPref;
+        private static MelonPreferences_Entry<int> _newUserColorRPref;
+        private static MelonPreferences_Entry<int> _newUserColorGPref;
+        private static MelonPreferences_Entry<int> _newUserColorBPref;
+        private static MelonPreferences_Entry<int> _visitorColorRPref;
+        private static MelonPreferences_Entry<int> _visitorColorGPref;
+        private static MelonPreferences_Entry<int> _visitorColorBPref;
+        private static MelonPreferences_Entry<int> _nuisanceColorRPref;
+        private static MelonPreferences_Entry<int> _nuisanceColorGPref;
+        private static MelonPreferences_Entry<int> _nuisanceColorBPref;
         private static MelonPreferences_Entry<bool> _reloadAvatar;
-        private static readonly List<APIUser> CachedApiUsers = new List<APIUser>();
-        private static readonly Queue<string> UsersToFetch = new Queue<string>();
-        private static readonly Random Random = new Random();
+        private static readonly List<APIUser> CachedApiUsers = new();
+        private static readonly Queue<string> UsersToFetch = new();
+        private static readonly Random Random = new();
 
+        private static MethodBase _showSocialRankMethod;
         private static MethodInfo _reloadAvatarMethod;
 
         public override void OnApplicationStart()
@@ -57,25 +71,37 @@ namespace OGTrustRanks
             var cat = MelonPreferences.CreateCategory("ogtrustranks", "OGTrustRanks");
             _enabledPref = cat.CreateEntry("enabled", true, "Enabled");
 
-            _knownColorRPref = cat.CreateEntry("KnownColorR", 255, "Red component of the Known color");
-            _knownColorGPref = cat.CreateEntry("KnownColorG", 123, "Green component of the Known color");
-            _knownColorBPref = cat.CreateEntry("KnownColorB", 66, "Blue component of the Known color");
+            _legendColorRPref = cat.CreateEntry("LegendColorR", 255, "Red component of the Legend color");
+            _legendColorGPref = cat.CreateEntry("LegendColorG", 105, "Green component of the Legend color");
+            _legendColorBPref = cat.CreateEntry("LegendColorB", 180, "Blue component of the Legend color");
 
-            _trustedColorRPref = cat.CreateEntry("TrustedColorR", 130, "Red component of the Trusted color");
-            _trustedColorGPref = cat.CreateEntry("TrustedColorG", 66, "Green component of the Trusted color");
-            _trustedColorBPref = cat.CreateEntry("TrustedColorB", 230, "Blue component of the Trusted color");
-
-            _veteranColorRPref = cat.CreateEntry("VeteranColorR", 255, "Red component of the Veteran color");
-            _veteranColorGPref = cat.CreateEntry("VeteranColorG", 208, "Green component of the Veteran color");
+            _veteranColorRPref = cat.CreateEntry("VeteranColorR", 255, "Red component of the Veteran user color");
+            _veteranColorGPref = cat.CreateEntry("VeteranColorG", 208, "Green component of the Veteran user color");
             _veteranColorBPref = cat.CreateEntry("VeteranColorB", 0, "Blue component of the Veteran color");
 
-            _legendaryColorRPref = cat.CreateEntry("LegendaryColorR", 255, "Red component of the Legendary color");
-            _legendaryColorGPref = cat.CreateEntry("LegendaryColorG", 105, "Green component of the Legendary color");
-            _legendaryColorBPref = cat.CreateEntry("LegendaryColorB", 180, "Blue component of the Legendary color");
+            _trustedColorRPref = cat.CreateEntry("TrustedColorR", 130, "Red component of the Trusted user color");
+            _trustedColorGPref = cat.CreateEntry("TrustedColorG", 66, "Green component of the Trusted user color");
+            _trustedColorBPref = cat.CreateEntry("TrustedColorB", 230, "Blue component of the Trusted user color");
 
-            _vrchatTeamColorRPref = cat.CreateEntry("VRChatTeamColorR", 255, "Red component of the VRChat Team color");
-            _vrchatTeamColorGPref = cat.CreateEntry("VRChatTeamColorG", 38, "Green component of the VRChat Team color");
-            _vrchatTeamColorBPref = cat.CreateEntry("VRChatTeamColorB", 38, "Blue component of the VRChat Team color");
+            _knownColorRPref = cat.CreateEntry("KnownColorR", 255, "Red component of the Known user color");
+            _knownColorGPref = cat.CreateEntry("KnownColorG", 123, "Green component of the Known user color");
+            _knownColorBPref = cat.CreateEntry("KnownColorB", 66, "Blue component of the Known user color");
+
+            _userColorRPref = cat.CreateEntry("UserColorR", 43, "Red component of the User color");
+            _userColorGPref = cat.CreateEntry("UserColorG", 207, "Green component of the User color");
+            _userColorBPref = cat.CreateEntry("UserColorB", 97, "Blue component of the User color");
+
+            _newUserColorRPref = cat.CreateEntry("NewUserColorR", 23, "Red component of the New user color");
+            _newUserColorGPref = cat.CreateEntry("NewUserColorG", 120, "Green component of the New user color");
+            _newUserColorBPref = cat.CreateEntry("NewUserColorB", 255, "Blue component of the New user color");
+
+            _visitorColorRPref = cat.CreateEntry("VisitorColorR", 204, "Red component of the Visitor color");
+            _visitorColorGPref = cat.CreateEntry("VisitorColorG", 204, "Green component of the Visitor color");
+            _visitorColorBPref = cat.CreateEntry("VisitorColorB", 204, "Blue component of the Visitor color");
+
+            _nuisanceColorRPref = cat.CreateEntry("NuisanceColorR", 120, "Red component of the Nuisance color");
+            _nuisanceColorGPref = cat.CreateEntry("NuisanceColorG", 47, "Green component of the Nuisance color");
+            _nuisanceColorBPref = cat.CreateEntry("NuisanceColorB", 47, "Blue component of the Nuisance color");
 
             _reloadAvatar = cat.CreateEntry("ReloadAvatar", false,
                 "Reload avatars when fetched rank to update colors for BTKANameplateMod");
@@ -101,6 +127,25 @@ namespace OGTrustRanks
                         BindingFlags.NonPublic | BindingFlags.Static)))
             );
 
+            _showSocialRankMethod = XrefScanner.XrefScan(friendlyNameTargetMethod).Single(x =>
+            {
+                if (x.Type != XrefType.Method)
+                    return false;
+
+                var m = x.TryResolve();
+                if (m == null || !m.IsStatic || m.DeclaringType != typeof(VRCPlayer))
+                    return false;
+
+                var asInfo = m as MethodInfo;
+                if (asInfo == null || asInfo.ReturnType != typeof(bool))
+                    return false;
+
+                if (m.GetParameters().Length != 1 && m.GetParameters()[0].ParameterType != typeof(APIUser))
+                    return false;
+
+                return XrefScanner.XrefScan(m).Count() > 1;
+            }).TryResolve();
+
             // Thanks loukylor
             _reloadAvatarMethod = typeof(VRCPlayer).GetMethods().First(mi =>
                 mi.Name.StartsWith("Method_Private_Void_Boolean_") && mi.Name.Length < 31 &&
@@ -113,9 +158,9 @@ namespace OGTrustRanks
 
         private IEnumerator InitializeNetworkHooks()
         {
-            while (ReferenceEquals(NetworkManager.field_Internal_Static_NetworkManager_0, null)) yield return null;
-            while (ReferenceEquals(VRCAudioManager.field_Private_Static_VRCAudioManager_0, null)) yield return null;
-            while (ReferenceEquals(VRCUiManager.prop_VRCUiManager_0, null)) yield return null;
+            while (NetworkManager.field_Internal_Static_NetworkManager_0 is null) yield return null;
+            while (VRCAudioManager.field_Private_Static_VRCAudioManager_0 is null) yield return null;
+            while (VRCUiManager.prop_VRCUiManager_0 is null) yield return null;
 
             NetworkManagerHooks.Initialize();
             NetworkManagerHooks.OnJoin += OnPlayerJoin;
@@ -152,12 +197,10 @@ namespace OGTrustRanks
                     APIUser.FetchUser(id, new Action<APIUser>(user =>
                     {
                         CachedApiUsers.Add(user);
-                        if (_reloadAvatar.Value)
-                        {
-                            var player = GetPlayerByUserId(id);
-                            _reloadAvatarMethod.Invoke(player._vrcplayer, new object[] { true });
-                        }
-                    }), new Action<string>(error => { MelonLogger.Error($"Could not fetch APIUser object of {id}"); }));
+                        if (!_reloadAvatar.Value) return;
+                        var player = GetPlayerByUserId(id);
+                        _reloadAvatarMethod.Invoke(player._vrcplayer, new object[] {true});
+                    }), new Action<string>(_ => { MelonLogger.Error($"Could not fetch APIUser object of {id}"); }));
                     yield return new WaitForSeconds(Random.Next(2, 5));
                 }
             }
@@ -181,21 +224,34 @@ namespace OGTrustRanks
         private static void Refresh()
         {
             UpdateColors();
-            SetupTrustRankButton();
+            // SetupTrustRankButton(); requi fix plz
         }
 
         private static void UpdateColors()
         {
-            _knownUserColor = new Color(_knownColorRPref.Value / 255.0f, _knownColorGPref.Value / 255.0f,
-                _knownColorBPref.Value / 255.0f);
-            _trustedUserColor = new Color(_trustedColorRPref.Value / 255.0f, _trustedColorGPref.Value / 255.0f,
-                _trustedColorBPref.Value / 255.0f);
+            _legendColor = new Color(_legendColorRPref.Value / 255.0f, _legendColorGPref.Value / 255.0f,
+                _legendColorBPref.Value / 255.0f);
+
             _veteranUserColor = new Color(_veteranColorRPref.Value / 255.0f, _veteranColorGPref.Value / 255.0f,
                 _veteranColorBPref.Value / 255.0f);
-            _legendColor = new Color(_legendaryColorRPref.Value / 255.0f, _legendaryColorGPref.Value / 255.0f,
-                _legendaryColorBPref.Value / 255.0f);
-            _vrchatTeamColor = new Color(_vrchatTeamColorRPref.Value / 255.0f, _vrchatTeamColorGPref.Value / 255.0f,
-                _vrchatTeamColorBPref.Value / 255.0f);
+
+            _trustedUserColor = new Color(_trustedColorRPref.Value / 255.0f, _trustedColorGPref.Value / 255.0f,
+                _trustedColorBPref.Value / 255.0f);
+
+            _knownUserColor = new Color(_knownColorRPref.Value / 255.0f, _knownColorGPref.Value / 255.0f,
+                _knownColorBPref.Value / 255.0f);
+
+            _userUserColor = new Color(_userColorRPref.Value / 255.0f, _userColorGPref.Value / 255.0f,
+                _userColorBPref.Value / 255.0f);
+
+            _newUserColor = new Color(_newUserColorRPref.Value / 255.0f, _newUserColorGPref.Value / 255.0f,
+                _newUserColorBPref.Value / 255.0f);
+
+            _visitorColor = new Color(_visitorColorRPref.Value / 255.0f, _visitorColorGPref.Value / 255.0f,
+                _visitorColorBPref.Value / 255.0f);
+
+            _nuisanceColor = new Color(_nuisanceColorRPref.Value / 255.0f, _nuisanceColorGPref.Value / 255.0f,
+                _nuisanceColorBPref.Value / 255.0f);
         }
 
         private static void SetupTrustRankButton()
@@ -211,8 +267,8 @@ namespace OGTrustRanks
                 return;
 
             var rank = GetTrustRankEnum(APIUser.CurrentUser);
-            if (rank == TrustRanks.Ignore)
-                return;
+
+            if (rank > TrustRanks.Known) return;
 
             if (_enabledPref.Value)
                 switch (rank)
@@ -254,38 +310,46 @@ namespace OGTrustRanks
 
         private static bool GetFriendlyDetailedNameForSocialRank(APIUser __0, ref string __result)
         {
-            if (__0 == null || !_enabledPref.Value) return true;
+            if (__0 == null || __0.IsSelf || !_enabledPref.Value) return true;
+
+            if (GetPlayerByUserId(__0.id) != null)
+            {
+                var showSocialRank = (bool) _showSocialRankMethod.Invoke(null, new object[] {__0});
+                if (!showSocialRank) return true;
+            }
 
             var apiUser = CachedApiUsers.Find(x => x.id == __0.id) ?? __0;
             var rank = GetTrustRankEnum(apiUser);
 
-            if (rank == TrustRanks.Ignore) return true;
-
-            __result = GetRank(apiUser, rank, ref __result);
+            __result = GetRank(apiUser, rank);
             return false;
         }
 
-        private static string GetRank(APIUser apiUser, TrustRanks rank, ref string __result)
+        private static string GetRank(APIUser apiUser, TrustRanks rank)
         {
-            if (rank <= (TrustRanks)3 && apiUser.HasTag("system_legend"))
-                return __result = $"{rank} User + Legend";
+            if (apiUser.HasTag("system_legend"))
+                return $"{rank} User + Legend";
 
-            if (rank == TrustRanks.VRChatTeam)
-                return __result = "VRChat Team";
-
-            return __result = $"{rank} User";
+            return rank is TrustRanks.User or TrustRanks.Visitor or TrustRanks.Nuisance
+                ? rank.ToString()
+                : $"{rank} User";
         }
 
 
         private static bool GetColorForSocialRank(APIUser __0, ref Color __result)
         {
-            if (__0 == null || !_enabledPref.Value || APIUser.IsFriendsWith(__0.id)) return true;
+            if (__0 == null || APIUser.IsFriendsWith(__0.id) || __0.IsSelf || !_enabledPref.Value) return true;
 
+            if (GetPlayerByUserId(__0.id) != null)
+            {
+                var showSocialRank = (bool) _showSocialRankMethod.Invoke(null, new object[] {__0});
+                if (!showSocialRank) return true;
+            }
 
             var apiUser = CachedApiUsers.Find(x => x.id == __0.id) ?? __0;
             var rank = GetTrustRankEnum(apiUser);
 
-            if (rank <= (TrustRanks)3 && apiUser.tags.Contains("system_legend"))
+            if (apiUser.HasTag("system_legend"))
             {
                 __result = _legendColor;
                 return false;
@@ -293,9 +357,6 @@ namespace OGTrustRanks
 
             switch (rank)
             {
-                case TrustRanks.VRChatTeam:
-                    __result = _vrchatTeamColor;
-                    return false;
                 case TrustRanks.Veteran:
                     __result = _veteranUserColor;
                     return false;
@@ -305,23 +366,26 @@ namespace OGTrustRanks
                 case TrustRanks.Known:
                     __result = _knownUserColor;
                     return false;
-                case TrustRanks.Ignore:
-                    break;
+                case TrustRanks.User:
+                    __result = _userUserColor;
+                    return false;
+                case TrustRanks.New:
+                    __result = _newUserColor;
+                    return false;
+                case TrustRanks.Visitor:
+                    __result = _visitorColor;
+                    return false;
+                case TrustRanks.Nuisance:
+                    __result = _nuisanceColor;
+                    return false;
                 default:
-                    throw new ArgumentOutOfRangeException();
+                    __result = _visitorColor;
+                    return false;
             }
-
-            return true;
         }
 
         private static TrustRanks GetTrustRankEnum(APIUser user)
         {
-            if (user?.tags == null || user.tags.Count <= 0)
-                return TrustRanks.Ignore;
-
-            if (user.hasSuperPowers || user.hasModerationPowers || user.hasVIPAccess || user.hasScriptingAccess)
-                return TrustRanks.VRChatTeam;
-
             if (user.hasLegendTrustLevel)
                 return TrustRanks.Veteran;
 
@@ -331,8 +395,21 @@ namespace OGTrustRanks
             if (user.hasTrustedTrustLevel)
                 return TrustRanks.Known;
 
-            return TrustRanks.Ignore;
+            if (user.hasKnownTrustLevel)
+                return TrustRanks.User;
+
+            if (user.hasBasicTrustLevel)
+                return TrustRanks.New;
+
+            if (user.HasTag(string.Empty) && !user.canPublishAvatars)
+                return TrustRanks.Visitor;
+
+            if (user.hasNegativeTrustLevel || user.hasVeryNegativeTrustLevel)
+                return TrustRanks.Nuisance;
+
+            return TrustRanks.Visitor;
         }
+
         private static Player GetPlayerByUserId(string userId)
         {
             foreach (var player in PlayerManager.field_Private_Static_PlayerManager_0.field_Private_List_1_Player_0)
@@ -343,11 +420,13 @@ namespace OGTrustRanks
 
         private enum TrustRanks
         {
-            Ignore,
-            Known,
-            Trusted,
             Veteran,
-            VRChatTeam
+            Trusted,
+            Known,
+            User,
+            New,
+            Visitor,
+            Nuisance
         }
     }
 }
